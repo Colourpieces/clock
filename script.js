2f01b30a8beb90778e2a79b6f4fa57eae4c14025
nb <github@n-baller.de>
2023-05-16 17:08:02 +0200

    added basics and digital time

---
+++ script.js
@@ -0,0 +1,32 @@
+const displayDigitalTime = document.querySelector("#display-clock-digital");
+function showDigitalTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const hoursBinary = makeTimeBinary(hours);
+  const minutesBinary = makeTimeBinary(minutes);
+  const secondsBinary = makeTimeBinary(seconds);
+
+  if (seconds % 2 === 0) {
+    displayDigitalTime.innerText =
+      hoursBinary + ":" + minutesBinary + ":" + secondsBinary;
+  } else {
+    displayDigitalTime.innerText =
+      hoursBinary + " " + minutesBinary + " " + secondsBinary;
+  }
+}
+
+// update time every 1000 ms (= 1 second)
+const intervalID = setInterval(showDigitalTime, 1000);
+
+function makeTimeBinary(time) {
+  let timeBinary = "";
+  if (time < 10) {
+    timeBinary = "0" + time;
+  } else {
+    timeBinary = time;
+  }
+  return timeBinary;
+}
