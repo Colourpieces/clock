1adb62291709c9f084df0cd957bc0df482e7740b
Nico König <nico.koenig@coding-bootcamps.eu>
2023-05-17 08:43:22 +0200

    refactor: refactors code

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
+const root = document.querySelector(":root");
 const displayDigitalTime = document.querySelector("#display-clock-digital");
-function showDigitalTime() {
+
+function setTime() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  const hoursBinary = makeTimeBinary(hours);
-  const minutesBinary = makeTimeBinary(minutes);
-  const secondsBinary = makeTimeBinary(seconds);
+  //analog time
+  root.style.setProperty(
+    "--degrees-hours",
+    Math.floor((hours * 360) / 12) + "deg"
+  );
+  root.style.setProperty(
+    "--degrees-minutes",
+    Math.floor((minutes * 360) / 60) + "deg"
+  );
+  root.style.setProperty(
+    "--degrees-seconds",
+    Math.floor((seconds * 360) / 60) + "deg"
+  );
+  //console.log(root.getAttribute("style"));
 
-  if (seconds % 2 === 0) {
-    displayDigitalTime.innerText =
-      hoursBinary + ":" + minutesBinary + ":" + secondsBinary;
-  } else {
-    displayDigitalTime.innerText =
-      hoursBinary + " " + minutesBinary + " " + secondsBinary;
-  }
+  //digital time
+  displayDigitalTimeString(hours, minutes, seconds);
 }
 
-// update time every 1000 ms (= 1 second)
-const intervalID = setInterval(showDigitalTime, 1000);
+function displayDigitalTimeString(hours, minutes, seconds) {
+  const separator = seconds % 2 === 0 ? ":" : " ";
+  hours = makeTimeBinary(hours);
+  minutes = makeTimeBinary(minutes);
+  seconds = makeTimeBinary(seconds);
+
+  displayDigitalTime.innerText =
+    hours + separator + minutes + separator + seconds;
+}
 
 function makeTimeBinary(time) {
+  // return time < 10 ? "0" + time : time.toString();
+
+  // return ("0" + time.toString()).slice(-2)
+
   let timeBinary = "";
   if (time < 10) {
     timeBinary = "0" + time;
   } else {
-    timeBinary = time;
+    timeBinary = time.toString();
   }
   return timeBinary;
 }
 
-const root = document.querySelector(":root");
-root.style.setProperty("--clr-red", pred);
+// update time every 1000 ms (= 1 second)
+const intervalID = setInterval(setTime, 1000);
